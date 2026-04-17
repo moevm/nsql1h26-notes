a070f8270317a90554c62aa0bdf6a95f3e1db337
ArtyomKF <Artemmailsbox@gmail.com>
2026-04-17 13:45:59 +0300

    create log schemas

---
+++ app/backend/log/log_schemas.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel, Field
+
+
+class LogBase(BaseModel):
+    action: str
+    
+
+class RegistrationLogBase(LogBase):
+    user_ref: str
+
+class RegistationLogCreate(RegistrationLogBase):
+    pass
+
+class RegistrationLogResponse(RegistrationLogBase):
+    log_key: str
+    created_at: str
+
+
+class PermissionLogBase(LogBase):
+    note_key: str
+    granted_by_key: str
+    granted_to_key: str
+    before_permission_type: str
+    after_permission_type: str
+
+class PermissionLogCreate(PermissionLogBase):
+    pass
+
+class PermissionLogResponse(PermissionLogBase):
+    log_key: str
+    created_at: str
+
+
+class NotesLogBase(LogBase):
+    note_key: str
+    user_key: str
+    state_before: str
+    state_after: str
+    diff: str
+
+class NotesLogCreate(NotesLogBase):
+    pass
+
+class NotesLogResponse(NotesLogBase):
+    log_key: str
+    created_at: str
