e9c47faf3579e7cf4c6153b7c8e411998c864893
Egor Anakhin <eanahin8v@mail.ru>
2026-04-19 18:53:59 +0300

    feat: updated notes requests params

--- app/client/src/entities/note/types/requests.ts
+++ app/client/src/entities/note/types/requests.ts
@@ -1,7 +1,13 @@
+import { ISOString } from "@/shared/types/date";
+
 export type GetNotesRequest = {
     parent_key?: string | null;
     tag?: string | null;
     search?: string | null;
+    created_from?: ISOString | null;
+    updated_from?: ISOString | null;
+    created_to?: ISOString | null;
+    updated_to?: ISOString | null;
     limit: number;
     offset: number;
 };
